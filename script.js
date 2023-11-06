0b2b19f495fa4557b6a0e29c0b6b038398e4c23a
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-06 16:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let heading = document.querySelector("h1");
+function onResize(){
+    let width = window.innerWidth;
+let height = window.innerHeight;
+heading.innerText = `Width: ${width} and Height: ${height}`;
+}
+onResize();
+
+window.addEventListener("resize", onResize);
